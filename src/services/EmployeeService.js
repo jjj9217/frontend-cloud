1981c3dc40fc990f683e0fb807efca7464923ed2
jjj9217 <136659619+jjj9217@users.noreply.github.com>
2024-04-23 15:20:15 +0900

    Update EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "https://api.jjj0.shop/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "https://backend.jjj0.shop/api/v1/employees";
 
 class EmployeeService {
 
